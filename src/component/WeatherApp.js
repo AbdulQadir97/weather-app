8b4b5093579982d01805d068ae12e9c97820f976
Abdul Qadir <aq201541@gmail.com>
2022-04-24 05:32:31 +0500

    update dynmically

--- src/component/WeatherApp.js
+++ src/component/WeatherApp.js
@@ -7,7 +7,8 @@ import SearchCity from './SearchCity'
 const WeatherApp = () => {
 
     const [inputValue, setInputValue] = useState('')
-    const [data, setData] = useState('')
+
+    const [data, setData] = useState([])
     const inputHandler = (e) => {
         setInputValue(e.target.value)
     }
@@ -23,18 +24,29 @@ const WeatherApp = () => {
         fetch(`${base}weather?q=${inputValue}&appid=${key}&units=metric`)
             .then((res) => {
                 res.json().then((response) => {
-                setData(response)
+                setData([response])
                 })
             })
 
     }
-
     console.log(data)
+
     return (
         <>
 
             <SearchCity className='w-100 inputFeild' value={inputValue} onChange={inputHandler} />
             <button className='btn btn-transparent w-50' onClick={fetchWeatherApi}>Search</button>
+            {
+                data.map((v, i)=>{
+                    return( 
+                    <div key={i}>
+                    <div className='container displayStyling'><h1>{v.name}</h1></div>    
+                    <div className='container displayStyling'><h1>{v.weather[0].main}</h1></div>    
+                    <div className='container displayStyling' ><h1 className='text-center'>{Math.round(v.main.temp)}&deg;C</h1></div>
+                    </div>)
+                })
+            }
+            
         </>
     )
 }
